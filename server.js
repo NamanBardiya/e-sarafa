9cedce0bbd3bbf817f0cdb2d6292174e750a75fb
Naman Bardiya <namanbardiyaskn@gmail.com>
2023-01-31 09:01:37 +0530

    database uri changed

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const dbConfig = require("./app/config/db.config");
+const dotenv = require('dotenv');
+dotenv.config();
+//console.log(`Your port is ${process.env.PORT}`);
 
 const app = express();
 
@@ -18,9 +21,12 @@ app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
 const Role = db.role;
+const Shop = db.shop;
+const User = db.user;
+const Loan = db.Loan;
 
 db.mongoose
-  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+  .connect(`mongodb+srv://NamanBardiya:${process.env.MONGO_URI}@cluster0.sp2zple.mongodb.net/e_sarafa_db?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
@@ -83,4 +89,55 @@ function initial() {
       });
     }
   });
+
+  User.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new User(
+     
+      {
+        "roles": [
+          
+        ],
+        "username": "naman test1",
+        "email": "nt1@gmail.com",
+        "password": "$2a$08$T3Jx2yIfEqpbIxQZOovehu5EJi.CihkZh7uRD7N26KMYJA53.ftlC",
+        "__v": 1
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added entry to user collection");
+      });
+
+      
+    }
+  });
+
+  Shop.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Shop(
+     
+        {
+          
+          "name": "shop 001",
+          "description": "Loans available with best interest rates here!",
+          "owners": [
+            "test1"
+          ],
+          "borrowers": [],
+          "loans": []
+        }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added entry to shop collection");
+      });
+
+      
+    }
+  });
+
+  
 }
